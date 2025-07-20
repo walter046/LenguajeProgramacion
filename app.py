abde785e6672fe23e941343c23637915c55eee6a
walter046 <165204329+walter046@users.noreply.github.com>
2025-07-19 19:19:43 -0500

    listo
    listo

--- app.py
+++ app.py
@@ -22,9 +22,6 @@ app.register_blueprint(user_bp)
 from routes.main import main_bp
 app.register_blueprint(main_bp)
 
-# Rutas de carrito
-from routes.cart import cart_bp
-app.register_blueprint(cart_bp)
 
 # Rutas de pedidos
 from routes.pedidos import pedidos_bp
@@ -39,8 +36,8 @@ from routes.products import products_bp
 app.register_blueprint(products_bp)
 
 
-from routes.cartv2 import cartv2_bp
-app.register_blueprint(cartv2_bp)
+from routes.cart import cart_bp
+app.register_blueprint(cart_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
